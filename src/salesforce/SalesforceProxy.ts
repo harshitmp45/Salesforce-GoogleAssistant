008ae81281ca4019d50372ba3e909bea295a85e3
harshitmp45 <46630754+harshitmp45@users.noreply.github.com>
2019-01-16 11:31:29 +0530

    Update SalesforceProxy.ts

--- src/salesforce/SalesforceProxy.ts
+++ src/salesforce/SalesforceProxy.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Report } from './../models/Report.interface';
 
-let proxyURL = 'https://harshitmp45-dev-ed.my.salesforce.com/services/apexrest';
+let proxyURL = 'https://harshitgoyal-developer-edition.ap5.force.com/services/apexrest';
 
 export class SalesforceProxy {
 
