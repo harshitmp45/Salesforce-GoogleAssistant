8fe4fad5c7151eaea9c9937f4b719c9c6c6e694d
Harshit Goyal <harshitmp45@gmail.com>
2019-01-17 00:11:45 +0530

    Update DialogFlowAction.interface.ts

--- src/models/DialogFlowAction.interface.ts
+++ src/models/DialogFlowAction.interface.ts
@@ -3,5 +3,6 @@ import { DialogflowApp } from 'actions-on-google';
 export interface DialogFlowAction {
   name: string,
   createrecordintent: string,
+  handlerRecord: (app: DialogflowApp) => void,
   handler: (app: DialogflowApp) => void
 }
