71a15844f2e08ac39c1dc21e3313b9721ce889c1
Harshit Goyal <harshitmp45@gmail.com>
2019-01-17 00:10:01 +0530

    Update ShowReportAction.ts

--- src/dialogflow/ShowReportAction.ts
+++ src/dialogflow/ShowReportAction.ts
@@ -11,27 +11,15 @@ export async function handler(dialogflowApp: DialogflowApp) {
 
   // Get the Report name and the Filter.
   let reportName = dialogflowApp.getArgument('reportName') + '';
-  let SObjectName = dialogflowApp.getArgument('SObjectName') + '';
-  let RecordName = dialogflowApp.getArgument('RecordName') + '';
-  let RecordField = dialogflowApp.getArgument('RecordField') + '';
-  let RecordValue = dialogflowApp.getArgument('RecordValue') + '';
   let filter = {
     column: dialogflowApp.getArgument('filterField') + '', 
     operator: dialogflowApp.getArgument('filterOperator') + '', 
     value: dialogflowApp.getArgument('filterValue') + ''
   };
-  if(reportName){
 	let report :Report  = <Report> {name: reportName, filter: filter};
 	  // Call Salesforce.
 	let response = await salesforceProxy.showReport(report);
     dialogflowApp.ask(response.data as string);
-  }
-  if(SObjectName){
-	let record :Record  = <Record> {sObjectName: SObjectName, recordName: RecordName,recordField: RecordField,recordValue: RecordValue};
-	  // Call Salesforce.
-	let response = await salesforceProxy.createupdateRercord(record);
-    dialogflowApp.ask(response.data as string);  
-  }
 }
 export async function handlerRecord(dialogflowApp: DialogflowApp) {
   let salesforceProxy = new SalesforceProxy();
