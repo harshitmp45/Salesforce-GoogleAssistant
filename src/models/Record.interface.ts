588956e2ccce468d0a236f21bc091ae475cdb58b
harshit.b.goyal <harshit.b.goyal@P5-GL6K882.dir.svc.accenture.com>
2019-01-17 13:48:00 +0530

    Changes

--- src/models/Record.interface.ts
+++ src/models/Record.interface.ts
@@ -1,10 +1,6 @@
 export interface Record {
-  name: string
-  filter?: Filter
-};
-
-interface Filter {
-  column: string,
-  operator: string,
-  value: string
+  sObjectNameinterface: string
+  recordNameinterface: string
+  recordFieldinterface: string
+  recordValueinterface: string
 };
